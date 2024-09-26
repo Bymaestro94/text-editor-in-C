3b35a38886d2593bf067ea0726c1f93132df4799
Bymaestro94 <bymaestrowork94@gamil.com>
2024-09-26 16:49:16 +0300

    third commit add rawmode

--- kilo.c
+++ kilo.c
@@ -1,3 +1,20 @@
+#include <termios.h>
+#include <unistd.h>
+
+void enableRawMode(){
+    struct termios raw;
+
+    tcgetattr(STDIN_FILENO, &raw);
+
+    raw.c_flag &= ~(ECHO);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main(){
+    enableRawMode()
+
+    char c;
+    while (read(STDIN_FILEND, &c, 1) == 1 && c != 'q');
     return 0;
 }
