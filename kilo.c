debf2ec1d67e65f30dcbb9554a7208d429d92ad2
Bymaestro94 <bymaestrowork94@gamil.com>
2024-09-27 23:24:12 +0300

    fix some bugs

--- kilo.c
+++ kilo.c
@@ -1,20 +1,30 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+struct termios orig_termios;
+
+void disableRawMode(){
+   tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void enableRawMode(){
-    struct termios raw;
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO | ICANON);
 
     tcgetattr(STDIN_FILENO, &raw);
 
-    raw.c_flag &= ~(ECHO);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main(){
-    enableRawMode()
+    enableRawMode();
 
     char c;
-    while (read(STDIN_FILEND, &c, 1) == 1 && c != 'q');
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
 }
